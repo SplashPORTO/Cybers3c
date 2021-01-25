5da01bf9379c2a3d7215de84620d883b71df5d6d
Nuno Splash <nuno.splash@gmail.com>
2021-01-25 19:09:13 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -4,67 +4,63 @@
 import os                       # Sistema Operativo
 import sys                      # Mexer no sistema
 import socket                   # Socket de Rede
-import threading                # Thread
-from datetime import datetime   # Para contagem do tempo
-
-
-#_ESPECIAL_  Importo um tipo de letra. Primeiro usei o pip3 para instalar este modulo
-import pyfiglet
+import threading                # multiThreading
+import datetime                 # Para contagem do tempo ao segundo
+import pyfiglet                 #_ESPECIAL_  Importo um tipo de letra. Primeiro usei o pip3 para instalar este modulo
 
 
 
 
 
-
-
-
-#__________________MAIN_____________________
-
-# Uso o tipo de font para escrever Port Scanner no ecra
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER") 
 print (ascii_banner) 
-
-# Programer
 print ("By: Pinto\n")  
-print ('') 
-#
-#  Pedir o IP da maquina destino fazer scanner
-destino = input('[+] Qual o IP da maquina> ')
+print ('')
 
-  
+#  Pedir o IP da maquina destino fazer scanner
+ip = input('[+] Qual o IP da maquina> ')
+
+
+
 # Damos feedback ao user do input
 print ('')
 print ("-" * 50) 
-print ("Scanning ao  TCP/IP: " + destino) 
-print ("Scanning a iniciar : " + str(datetime.now())) 
+print ("Scanning ao  TCP/IP: " + ip) 
+print ("Scanning a iniciar : " + str(datetime.datetime.now())) 
 print ("-" * 50)
 print ('')
 
+def portScanner (ip,port):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                   #Criar aligação via socket
+            pp = s.connect_ex((ip, port))
+            if pp == 0:
+                print ("Porta " +str(port) + "  ::::: Está Aberta")                #Validar se a porta esta Aberta
+            else:
+                print ("Porta " +str(port) + "  ::::: Está Fechada")               #Validar se a porta esta Fecahda
+                s.close()
+
+        #Saidas do programa        
+        except socket.gaierror: 
+            print ("\n Não foi identificado o IP da Maquina !!!") 
+        except socket.error: 
+            print ("\n MOcorreu um erro de Comunicaçao !!!")
+        except KeyboardInterrupt: 
+            print ("\n Saiu do Programa | Scanner !!!")
+        except:
+            print ("Ocorreu um erro !!!")
+    
 
 
-# Definimos a maquina a inspecionar
-def portscanner (destino,porta):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        #Criar aligação via socket
-        Resultado = sock.connect_ex (destino, porta)                    #Conectar o socket ao destino
-        t0 = datetime.datetime.now()
-        for porta in range (1,1025):
-            thread = threading.Thread(target = portscanner, args=(destino,porta))
-            thread.start()
-            thread.join()
-            t1 = datetime.datetime.now () -t0
-            print ('Demorou' +str(t1)+ "segundos")
 
-        if Resultado == 0:
-            print ("Porta " +str(porta) + "Está Aberta")                #Validar se a porta esta aberta
-        else:
-            print ("Porta " +str(porta) + "Está Fechada")               #Apresentar o Resultado
-            sock.close()
 
-    #Saidas do programa         
-    except KeyboardInterrupt: 
-        print ("\n Saiu do Programa | Scanner!!!!") 
-        sys.exit() 
-    except socket.error: 
-        print ("\n Maquina não activa !!!!") 
-        sys.exit()
+
+# #__________________MAIN_____________________
+def main():
+    t0 = datetime.datetime.now()
+    for port in range(80, 111):
+        portScanner(ip,port)
+        t1 = datetime.datetime.now() -t0 
+        print ('Demorou' +str(t1)+ "segundos\n") 
+    
+main()
